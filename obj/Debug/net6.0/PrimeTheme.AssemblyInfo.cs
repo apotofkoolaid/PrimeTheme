1a4524c6d7a0b0912b99b6bb30db6bf54b643b20
apotofkoolaid <72956636+apotofkoolaid@users.noreply.github.com>
2024-06-23 20:14:32 -0500

    vs

--- obj/Debug/net6.0/PrimeTheme.AssemblyInfo.cs
+++ obj/Debug/net6.0/PrimeTheme.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeTheme")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33b3dbc2324664cf196dfa7925373dc64367cbba")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeTheme")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeTheme")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
