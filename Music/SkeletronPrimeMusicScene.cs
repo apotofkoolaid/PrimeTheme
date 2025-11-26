4809f106e5025cb0ac5f8dce3c77678f946e18eb
apotofkoolaid <72956636+apotofkoolaid@users.noreply.github.com>
2025-11-25 20:47:30 -0600

    v2.3

--- Music/SkeletronPrimeMusicScene.cs
+++ Music/SkeletronPrimeMusicScene.cs
@@ -7,27 +7,19 @@ namespace PrimeTheme.Music
 	public class SkeletronPrimeMusicScene : ModSceneEffect
 	{
 		//Enter the path to your music file here.
-		//Avoid using .wav files since they're generally discouraged for use in Terraria modding.
 		public override int Music => MusicLoader.GetMusicSlot(Mod, "Music/SkeletronPrime");
 
-        //Enter the music priority here. Values are: BiomeLow, BiomeMedium, BiomeHigh, Environment, Event, BossLow, BossMedium, and BossHigh.
-        //It typically looks like this:
-        //public override SceneEffectPriority Priority => SceneEffectPriority.BossLow;
-
-        //...but the popular Calamity Mod changes Skeletron Prime's music to Boss 3 when master and revengeance mode are active, so in this instance it looks like this:
-        public override SceneEffectPriority Priority
-        {
-            get
-            {
-                var calamityMod = ModLoader.TryGetMod("CalamityMod", out Mod calamity);
-                return calamityMod && Main.masterMode && (bool)calamity.Call("DifficultyActive", "revengeance") ? SceneEffectPriority.BossMedium : SceneEffectPriority.BossLow;
-            }
-        }
+        //Enter a music priority here.
+		//Ordered values are: BiomeLow, BiomeMedium, BiomeHigh, Environment, Event, BossLow, BossMedium, and BossHigh.
+        public override SceneEffectPriority Priority => SceneEffectPriority.BossLow;
 
         public override bool IsSceneEffectActive(Player player)
 		{
-			//Enter whatever conditions you're wanting for your song down below.
-			return NPC.AnyNPCs(NPCID.SkeletronPrime) && Main.npc[NPC.FindFirstNPC(NPCID.SkeletronPrime)].Distance(player.Center) <= 8500f;
+            //Since this is a vanilla-style theme, I'm going to have this track be disabled when otherworldly music is on.
+            bool otherworldlyMusicIsOff = Main.swapMusic == Main.drunkWorld;
+
+            //Enter whatever conditions you're wanting for your song down here.
+            return otherworldlyMusicIsOff && NPC.AnyNPCs(NPCID.SkeletronPrime) && Main.npc[NPC.FindFirstNPC(NPCID.SkeletronPrime)].Distance(player.Center) <= 8500f;
 		}
 	}
 }
